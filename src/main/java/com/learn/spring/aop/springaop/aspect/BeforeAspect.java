763d5bbd3bb74cc5f1fff2b5fd4a805266e66beb
Nguyen Van Phuc <phucnv9985@yahoo.com>
2019-02-17 21:36:36 +0700

    @Before aspect

---
+++ src/main/java/com/learn/spring/aop/springaop/aspect/BeforeAspect.java
@@ -0,0 +1,23 @@
+package com.learn.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+// AOP
+// Configuration
+@Aspect
+@Configuration
+public class BeforeAspect {
+	
+    private Logger logger = LoggerFactory.getLogger(BeforeAspect.class);
+
+    //execution(* PACKAGE.*.*(..))
+    @Before("execution(* com.learn.spring.aop.springaop.business.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+    	logger.info(" Intercepted method calls - {}", joinPoint);
+    }
+}
