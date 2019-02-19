f9075f7977c9e67188300212d962b1f312e9040a
phuc.nguyenvan <phucnv9985@yahoo.com>
2019-02-19 17:39:02 +0700

    Best Practice : Use common Pointcut Configuration

--- src/main/java/com/learn/spring/aop/springaop/aspect/BeforeAspect.java
+++ src/main/java/com/learn/spring/aop/springaop/aspect/BeforeAspect.java
@@ -16,8 +16,8 @@ public class BeforeAspect {
     private Logger logger = LoggerFactory.getLogger(BeforeAspect.class);
 
     //execution(* PACKAGE.*.*(..))
-    @Before("execution(* com.learn.spring.aop.springaop.business.*.*(..))")
+    @Before("com.learn.spring.aop.springaop.aspect.CommonJoinPointConfig.dataLayerExecution()")
     public void before(JoinPoint joinPoint) {
-    	logger.info(" Intercepted method calls - {}", joinPoint);
+    	logger.info(" Intercepted method calls using dataLayerExecution - {}", joinPoint);
     }
 }
---
+++ src/main/java/com/learn/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -0,0 +1,9 @@
+package com.learn.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class CommonJoinPointConfig {
+
+	@Pointcut("execution(* com.learn.spring.aop.springaop.data.*.*(..))")
+	public void dataLayerExecution() {}
+}
